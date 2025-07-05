ceee689892f0beda036ceeafdc5039338167f513
divyanshu verma <vikashsrivastav2006@gmail.com>
2025-07-05 13:21:32 +0530

    Add files via upload

---
+++ JS FILES/new.js
@@ -0,0 +1,20 @@
+let a = 1;
+let b = "divyanshu";
+let c = true;
+let d = null;
+
+let o = {
+ name : "divyanshu",
+ age : "15" ,
+}
+
+console.log(o);
+console.log(typeof o);
+
+const item = 
+{
+    age: "12",
+    class: "6",
+    roll: "1",
+}
+console.log(item["age"]);
